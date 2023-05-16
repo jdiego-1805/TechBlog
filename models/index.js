9df921891db533bfb6e7f9a7bfc9712a14e7d510
jdiego-1805 <63172283+jdiego-1805@users.noreply.github.com>
2023-05-15 23:06:30 -0500

    added comments file

--- models/index.js
+++ models/index.js
@@ -1,7 +1,13 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
@@ -10,4 +16,8 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog, Comment };
